b43dc85daf98fae69b91802f01b0eaff0d4ddb9d
Ryan <ryan.3feitosa@gmail.com>
2024-12-04 18:06:19 -0300

    Stro e tororó

---
+++ src/main/java/facul/multi/SawAcade/SawAcadeApplication.java
@@ -0,0 +1,13 @@
+package facul.multi.SawAcade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SawAcadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SawAcadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/facul/multi/SawAcade/SawAcadeApplicationTests.java
@@ -0,0 +1,13 @@
+package facul.multi.SawAcade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SawAcadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
